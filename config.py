5949dffe22c64e5e25d6f4390950eae955e7dc61
Onserioo0 <joshuaomirera@gmail.com>
2024-04-11 10:00:35 +0300

    .

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 
-DB_HOST = 'DESKTOP-9MH94OT\\SQLEXPRESS' 
+DB_HOST = 'DESKTOP-RTQKU4O' 
 #DB_USER = 'smartenduser'  # SQL Server username
 #DB_PASSWORD = 'l0l0t1ng@2209'  # SQL Server password
 DB_NAME = 'StaffDB'
